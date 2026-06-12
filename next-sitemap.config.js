ce45cd6df765d88f16c9ce6f55d92b3b53b357eb
Rafał Haduch <rafalhofficial@gmail.com>
2026-06-12 23:21:06 +0200

    feat: add utility packages and developer tools
    prettier, prettier-plugin-tailwindcss, eslint-config-prettier, next-sitemap, lucide-react, tailwind-merge, clsx

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    siteUrl: 'https://example.com', //TODO:  change to imported const
+    generateRobotsTxt: true,
+    sitemapSize: 50000,
+    changefreq: 'monthly',
+    priority: 0.7,
+};
