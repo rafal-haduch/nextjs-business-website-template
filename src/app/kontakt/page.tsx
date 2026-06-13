398be1ab9d563ad0c7e338c70d4e001619fe50a7
Rafał Haduch <rafalhofficial@gmail.com>
2026-06-13 04:12:27 +0200

    feat: add base subpages with wip section

---
+++ src/app/kontakt/page.tsx
@@ -0,0 +1,13 @@
+import Section from '@/src/components/section';
+import Container from '@/src/components/container';
+import WorkInProgress from '@/src/components/wip';
+
+export default function ContactPage() {
+    return (
+        <Section spacing="single">
+            <Container>
+                <WorkInProgress />
+            </Container>
+        </Section>
+    );
+}
