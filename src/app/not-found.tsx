7782cb0aedfec73f4f92f8b790e0c1636656bf71
Rafał Haduch <rafalhofficial@gmail.com>
2026-06-13 04:12:58 +0200

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+import { ROUTES } from '@/src/constants/routes';
+
+import Section from '@/src/components/section';
+import Container from '@/src/components/container';
+
+export default function NotFound() {
+    return (
+        <Section spacing="single" aria-labelledby="not-found-heading">
+            <Container className="text-center">
+                <h1 id="not-found-heading" className="mb-6">
+                    Nie znaleziono treści
+                </h1>
+                <p className="mb-8">Przejdź do strony głównej lub skorzystaj z menu na górze.</p>
+                <Link href={ROUTES.HOME.path} className="btn btn-lg btn-primary">
+                    Strona główna{' '}
+                </Link>
+            </Container>
+        </Section>
+    );
+}
